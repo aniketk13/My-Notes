db6d36c839b18c2a89d4e72c634fc715e546ecb4
Aniket Khajanchi <ani.khajanchi257@gmail.com>
2022-01-18 22:07:30 +0530

    removed button from action bar

--- app/src/main/java/com/example/mynotes/MainFragment.kt
+++ app/src/main/java/com/example/mynotes/MainFragment.kt
@@ -6,6 +6,7 @@ import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.NavDirections
@@ -25,6 +26,7 @@ class MainFragment : Fragment(),NotesListAdapter.ListItemListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(false)
         binding= MainFragmentBinding.inflate(inflater,container,false)
         viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         with(binding.recyclerView){
